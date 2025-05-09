d2373dc0db152eb5e64e6580eed69add2c8ab4c3
Manuel López <manu3llop3z@gmail.com>
2025-05-09 11:02:11 +0200

    fix: añadir el bean getPriceService para permitir la inyección mediante Spring

--- src/main/java/dev/mlopez/prueba/inditex/prices/infrastructure/config/ApplicationConfig.java
+++ src/main/java/dev/mlopez/prueba/inditex/prices/infrastructure/config/ApplicationConfig.java
@@ -1,2 +1,16 @@
-package dev.mlopez.prueba.inditex.prices.infrastructure.config;public class ApplicationConfig {
+package dev.mlopez.prueba.inditex.prices.infrastructure.config;
+
+import dev.mlopez.prueba.inditex.prices.application.GetPriceService;
+import dev.mlopez.prueba.inditex.prices.domain.port.PriceRepositoryPort;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ApplicationConfig {
+
+    @Bean
+    public GetPriceService getPriceService(PriceRepositoryPort priceRepositoryPort) {
+        return new GetPriceService(priceRepositoryPort);
+    }
+
 }
